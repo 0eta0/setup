f0791b202805b87293945fc923e6a22f3d7982df
0eta0 <eta.develop@gmail.com>
2020-12-23 18:50:57 +0900

    add: ruby and rust

--- startdash.zsh
+++ startdash.zsh
@@ -31,6 +31,14 @@ asdf global python 3.9.1 2.7.18
 asdf plugin-add golang https://github.com/kennyp/asdf-golang.git
 asdf install golang 1.15.6
 asdf global golang 1.15.6
+## ruby
+asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git
+asdf install ruby 2.7.2
+asdf global ruby 2.7.2
+## rust
+asdf plugin-add rust https://github.com/code-lever/asdf-rust.git
+asdf install rust 1.48.0 -y
+asdf global rust 1.48.0
 
 ## Source
 source ~/.zprofile
